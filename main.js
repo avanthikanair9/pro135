67373ebdfdbf14601e5e37a7fbc84fd17f81701a
avanthikanair9 <75365203+avanthikanair9@users.noreply.github.com>
2021-08-18 18:25:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded()
+{
+ console.log('Model Loaded!');
+ }
